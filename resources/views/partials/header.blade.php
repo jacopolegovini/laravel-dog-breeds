c0eea8a732924568c14b35e9dfbce054af5708fe
jacopolegovini <legovini96@gmail.com>
2024-11-01 11:39:38 +0100

    header added

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,5 +1,8 @@
-<header>
-    <h1>
-        Header
-    </h1>
+<header class="d-flex align-items-center">
+    <div class="header-general container d-flex justify-content-between align-items-center">
+        <h1>
+            Dog's Breed
+        </h1>
+        <a href="#">New breed</a>
+    </div>
 </header>
